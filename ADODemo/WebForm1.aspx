a6b9ea57efea4960d761fdf93f40f52e11d82269
ADMIN <ADMIN@DESKTOP-JG8PD29>
2024-11-05 11:58:25 +0530

    Add project files.

---
+++ ADODemo/WebForm1.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ADODemo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <center>
+    <form id="form1" runat="server">
+        <div class="container-fluid " >
+    
+        <asp:Label ID="Label4" runat="server" Text="Search Id:"></asp:Label>
+        <asp:TextBox ID="Txtid" runat="server" style="margin-left: 36px" Width="61px"></asp:TextBox>
+    
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="BtnSearch" runat="server" OnClick="BtnSearch_Click" Text="Search" />
+            <br />
+            <br />
+    
+    </div>
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="Student Name:"></asp:Label>
+        <asp:TextBox ID="Txtname" runat="server" style="margin-left: 57px" Width="175px"></asp:TextBox>
+    
+    </div>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Student Email:"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="Txtemail" runat="server" style="margin-left: 23px" Width="177px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label3" runat="server" Text="Student Address:"></asp:Label>
+            <asp:TextBox ID="Txtaddress" runat="server" style="margin-left: 48px" Width="177px"></asp:TextBox>
+        </p>
+        <asp:Button ID="Btncreate" runat="server" OnClick="Btncreate_Click" Text="Create" style="margin-left: 0px" Width="77px" />
+         <asp:Button ID="BtnUpdate" runat="server" OnClick="BtnUpdate_Click" Text="Update" style="margin-left: 0px" Width="88px" />
+        <asp:Button ID="Btndelete" runat="server" OnClick="Btndelete_Click" Text="Delete" style="margin-left: 0px" Width="95px" />
+        <asp:Button ID="Btndisplay" runat="server" OnClick="Btndisplay_Click" Text="Display" style="margin-left: 0px" Width="82px" />
+
+    
+        <div>
+    <asp:GridView ID="GridView1" runat="server">
+    </asp:GridView>
+            </div>
+        </form>
+        </center>
+    
+</body>
+</html>
---
+++ ADODemo/WebForm1.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADODemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string str = @"Data Source=DESKTOP-JG8PD29\SQLEXPRESS;Initial Catalog=AdoDemo;Integrated Security=True";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void clear()
+        {
+            Txtid.Text = "";
+            Txtname.Text = "";
+            Txtemail.Text = "";
+            Txtaddress.Text = "";
+        }
+
+        protected void Btncreate_Click(object sender, EventArgs e)
+        {
+            using(SqlConnection con=new SqlConnection(str))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO Student(Name,Email,Address)values(@Name,@Email,@Address)",con);
+                cmd.Parameters.AddWithValue("@Name", Txtname.Text.Trim());
+                cmd.Parameters.AddWithValue("@Email", Txtemail.Text.Trim());
+                cmd.Parameters.AddWithValue("@Address", Txtaddress.Text.Trim());
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('Information Submitted')</script>");
+                clear();
+                
+            }
+
+        }
+
+        protected void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            using(SqlConnection con=new SqlConnection(str))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("UPDATE Student set Name=@Name,Email=@Email,Address=@Address where Id='"+Txtid.Text+"'", con);
+                cmd.Parameters.AddWithValue("@Name", Txtname.Text.Trim());
+                cmd.Parameters.AddWithValue("@Email", Txtemail.Text.Trim());
+                cmd.Parameters.AddWithValue("@Address", Txtaddress.Text.Trim());
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('Information Submitted')</script>");
+                clear();
+
+            }
+        }
+
+        protected void BtnSearch_Click(object sender, EventArgs e)
+        {
+            using(SqlConnection con=new SqlConnection(str))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Student where Id like'" + Txtid.Text + "%'", con);
+                SqlDataReader sdr = cmd.ExecuteReader();
+                while(sdr.Read())
+                {
+                    Txtname.Text = sdr.GetValue(1).ToString();
+                    Txtemail.Text = sdr.GetValue(2).ToString();
+                    Txtaddress.Text = sdr.GetValue(3).ToString();
+                }
+                con.Close();
+
+                
+            }
+        }
+
+        protected void Btndelete_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(str))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("DELETE FROM Student where Id like'" + Txtid.Text + "'",con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('Information Deleted')</script>");
+                clear();
+            }
+        }
+
+        protected void Btndisplay_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(str))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Student",con);
+                SqlDataReader sdr = cmd.ExecuteReader();
+                GridView1.DataSource = sdr;
+                GridView1.DataBind();
+                con.Close();
+            }
+
+        }
+    }
+}
